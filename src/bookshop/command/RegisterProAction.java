50e33fdf04d968d599ebbf87fe04e645f926ac41
lyn921105 <yln92@naver.com>
2020-03-06 11:13:26 +0900

    all

---
+++ src/bookshop/command/RegisterProAction.java
@@ -0,0 +1,35 @@
+package bookshop.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sun.jmx.snmp.Timestamp;
+
+import bookshop.bean.LogonDataBean;
+import bookshop.bean.LogonDBBean;
+
+public class RegisterProAction implements CommandAction {
+
+	@Override
+	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		// TODO Auto-generated method stub
+		
+		request.setCharacterEncoding("utf-8");
+		
+		// 회원 가입 정보
+		LogonDataBean member = new LogonDataBean();
+		member.setId(request.getParameter("id"));
+		member.setPasswd(request.getParameter("passwd"));
+		member.setName(request.getParameter("name"));
+		member.setReg_date(new Timestamp(System.currentTimeMillis()));
+		member.setAddress(request.getParameter("address"));
+		member.setTel(request.getParameter("tel"));
+		
+		// 회원 가입 처리
+		LogonDBBean dbPro = LogonDBBean.getInstance();
+		dbPro.insertMember(member);
+		
+		return "/member/registerPro.jsp";
+	}
+
+}
